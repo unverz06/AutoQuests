9a53e2f78bfa890011be20119275ceb1f90ef5e4
unverz06 <contact.autoquest.unverz06@gmail.com>
2022-10-14 15:38:38 +0200

    fix(): msg cmd

--- help.lua
+++ help.lua
@@ -4,7 +4,7 @@ SLASH_HELP1 = "/aq-help"
 local color = "cffF08080";
 local msg_help = "\124" .. color .. "AutoQuests — Commands\124r"
 local cmd_help = "\124" .. color .. "Help : /aq-help\124r"
-local cmd_aaq = "\124" .. color .. "Abandon all quests : /aq-abandonAutoQuests\124r"
+local cmd_aaq = "\124" .. color .. "Abandon all quests : /aq-abandon\124r"
 
 -- Var
 local function AutoQuestHelper()
