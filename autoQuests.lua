39bea3bfbfe088e3ca5832ef117194c4ef769d76
unverz06 <contact.autoquest.unverz06@gmail.com>
2022-10-21 21:51:14 +0200

    fix(): gossip num option

--- autoQuests.lua
+++ autoQuests.lua
@@ -28,7 +28,7 @@ AQ_EventFrame:SetScript("OnEvent", function(self, event, ...)
     local npcGossipOptionsNumbers = #C_GossipInfo.GetOptions() -- Shadowlands and more
     local npcGossipOptionsNumbersClassic = C_GossipInfo.GetNumOptions() -- Retro compatibility (Classic)
 
-    -- print("\124" .. color_debug .. "AutoQuests Log — Available Quests: " .. npcGossipQuestAvailableCount .. ". Quests in progress: " .. npcGossipQuestCompleteCount .. ". Available Options: " .. npcGossipOptionsNumbers .. ". Gossip Type: " .. npcGossipType .. ".\124r") -- DEBUG
+    -- print("\124" .. color_debug .. "AutoQuests Log:\nAvailable Quests: " .. npcGossipQuestAvailableCount .. ".\nQuests in progress: " .. npcGossipQuestCompleteCount .. ".\nAvailable Options: " .. npcGossipOptionsNumbers .. ".\nAvailable Options Classic: " .. npcGossipOptionsNumbersClassic .. ".\124r") -- DEBUG
 
     if (npcGossipOptionsNumbers > 0 or npcGossipOptionsNumbersClassic > 0) then 
       -- if Gossip have choice (banker, battlemaster, binder, gossip, healer, petition, tabard, taxi, trainer, unlearn, or vendor)
@@ -51,7 +51,7 @@ AQ_EventFrame:SetScript("OnEvent", function(self, event, ...)
     local npcAvailableQuestCount = GetNumAvailableQuests()
     local npcActiveQuestCount = GetNumActiveQuests()
 
-    -- print("\124" .. color_debug .. "AutoQuests Log — Available: " .. npcAvailableQuestCount .. ". In progress: " .. npcActiveQuestCount ..".\124r") -- DEBUG
+    -- print("\124" .. color_debug .. "AutoQuests Log:\nAvailable: " .. npcAvailableQuestCount .. ".\nIn progress: " .. npcActiveQuestCount ..".\124r") -- DEBUG
 
     if (npcAvailableQuestCount > 0) then
       for i = 1, GetNumAvailableQuests() do
@@ -65,23 +65,23 @@ AQ_EventFrame:SetScript("OnEvent", function(self, event, ...)
   end
 
   if (event=="QUEST_FINISHED") then
-    -- print("\124" .. color_debug .. "AutoQuests Log — Is QUEST_FINISHED event.\124r") -- DEBUG
+    -- print("\124" .. color_debug .. "AutoQuests Log:\nIs QUEST_FINISHED event.\124r") -- DEBUG
   end
 
   if (event=="QUEST_DETAIL") then
-    -- print("\124" .. color_debug .. "AutoQuests Log — Is QUEST_DETAIL event.\124r") -- DEBUG
+    -- print("\124" .. color_debug .. "AutoQuests Log:\nIs QUEST_DETAIL event.\124r") -- DEBUG
     AcceptQuest()
   end
 
   if (event=="QUEST_PROGRESS") then
-    -- print("\124" .. color_debug .. "AutoQuests Log — Is QUEST_PROGRESS event.\124r") -- DEBUG
+    -- print("\124" .. color_debug .. "AutoQuests Log:\nIs QUEST_PROGRESS event.\124r") -- DEBUG
     CompleteQuest()
   end
 
   if (event=="QUEST_COMPLETE") then
     local npcQuestRewardsCount = GetNumQuestChoices()
 
-    -- print("\124" .. color_debug .. "AutoQuests Log — Reward count: " .. npcQuestRewardsCount .. ".\124r") -- DEBUG
+    -- print("\124" .. color_debug .. "AutoQuests Log:\nReward count: " .. npcQuestRewardsCount .. ".\124r") -- DEBUG
 
     if (npcQuestRewardsCount > 1) then
       print(msg_aq_reward)
