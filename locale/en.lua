0f069dade06dc734024d2f0eaacc47ed031d6011
unverz06 <>
2022-10-26 00:02:35 +0200

    feat(): locale translation init

---
+++ locale/en.lua
@@ -0,0 +1,21 @@
+local _, L = ...;
+
+L.WELCOME = {
+  TEXT_1 = "Hey ",
+  TEXT_2 = ", AutoQuests is loaded.",
+}
+
+L.TITLE = "AutoQuests — "
+L.REWARD = ", Choose your reward and complete your quest."
+L.BACK = "\n"
+
+L.DEBUG = {
+  LOG = "AutoQuests Log: ",
+  AVAILABLE = "Available Quests: ",
+  PROGRESS = "Quests in progress: ",
+  OPTION = "Available Options: ",
+  OPTIONC = "Available Options Classic: ",
+  REWARDCOUNT = "Reward count: ",
+  ISDETAIL = "Is QUEST_DETAIL event",
+  ISPROGRESS = "Is QUEST_PROGRESS event",
+}
