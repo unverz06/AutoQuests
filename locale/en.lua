c7b651d81dbe381a811b197d8168735f24075d34
unverz06 <>
2022-11-03 18:37:59 +0100

    chore(): lang

--- locale/en.lua
+++ locale/en.lua
@@ -3,10 +3,3 @@ local _, L = ...;
 L.TITLE = "AutoQuests — "
 L.REWARD = ", Choose your reward and complete your quest."
 L.BACK = "\n"
-
-L.OPTIONS = {
-  THANKS = "Thanks to use Autoquests.",
-  TICKET = "If you have bug, please comment on curseforge",
-  SUPPORT = "All donations via Paypal are welcome to keep this project going!",
-  COMMAND = ""
-}
--- locale/fr.lua
+++ locale/fr.lua
@@ -5,11 +5,5 @@ if GetLocale() == "frFR" then
   L.TITLE = "AutoQuests — "
   L.REWARD = ", Choisissez votre récompense et terminez votre quête."
   L.BACK = "\n"
-  
-  L.OPTIONS = {
-    THANKS = "Merci d'utiliser Autoquests.",
-    TICKET = "Si vous avez un bug, merci de laisser un commentaire sur Curseforge",
-    SUPPORT = "Toutes donation via Paypal est la bienvenue pour que ce projet perdure !"
-  }
 
 end
