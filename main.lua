b2fc9d6a2d4446602419b8eff2ab98a0c6aabb8d
unverz06 <>
2024-09-02 10:45:40 +0200

    change(): code refactor

--- main.lua
+++ main.lua
@@ -1,182 +1,160 @@
---[[
-    Variable
+--[[ 
+    Importation de la table de localisation 
 ]]
+local _, L = ...
 
-local _, L = ...;
+local playerName = UnitName("player")
+local defaultRewardIndex = 1
 
-local player = UnitName("player")
-local default = 1;
-
---[[
-    Misc function
+--[[ 
+    Fonction d'affichage de message 
 ]]
-
-local function selfMessage(v)
-  DEFAULT_CHAT_FRAME:AddMessage(v, 1.0, 1.0, 0.0);
+local function printMessage(message)
+    DEFAULT_CHAT_FRAME:AddMessage(message, 1.0, 1.0, 0.0)
 end
 
---[[
-    AutoQuests
+--[[ 
+    Fonction principale pour gérer les événements liés aux quêtes 
 ]]
-
-local function RegisterAutoQuestsEvents()
-  L.status = true
-  selfMessage(L.TITLE .. L.ENABLE)
-  local function Autoquests(self, event, key, down, ...)
-
-    if L.status == false or IsShiftKeyDown() or IsControlKeyDown() or IsAltKeyDown() then
-      -- Nothing ... Disabled Autoquests
-    else
-    
-      if (event=="GOSSIP_SHOW") then
-
-        -- GetactiveQuetes
-        local nActive = C_GossipInfo.GetNumActiveQuests()
-        local activeQuests = C_GossipInfo.GetActiveQuests()
-        -- GetavailableQuests
-        local nAvailable = C_GossipInfo.GetNumAvailableQuests()
-        local availableQuests = C_GossipInfo.GetAvailableQuests()
-        -- GetOptions
-        local nOptions = #C_GossipInfo.GetOptions()
-        local options = C_GossipInfo.GetOptions()
-        -- specificVar
-        local autoquestsQuestID = 0
-        local autoquestsIsComplete = 0
-        local autoquestsRepeatable = false
-        local autoquestsOptions = 0
-
-        --[[ Gossip event with the flag "💬(quest)" ]]
-        if nOptions == 1 then
-          -- local options = C_GossipInfo.GetOptions()
-          if options[1].flags == 1 then -- If it's a conversation with a quest (always choose the first one)
-            C_GossipInfo.SelectOption(options[1].gossipOptionID)
-          else
-            -- Nothing ... just reading the conversation
-          end
-        end
-        if nOptions > 1 then
-          for i = 1, nOptions do
-            if options[i].flags == 1 then -- If it's a conversation with a quest (always choose the first one)
-              autoquestsOptions = autoquestsOptions + 1;
-            end
-          end
-          if autoquestsOptions > 1 then
-            selfMessage(L.TITLE .. player .. L.GOSSIP);
-            PlaySound(5274, "master")
-          else
-            -- Nothing ... just reading the conversation
-          end
-        end
-
-        --[[ Gossip event with real quest "?!" ]]
-        if nAvailable > 0 then
-          for i = 1, nAvailable do
-            if type(availableQuests) == "table" then
-              autoquestsQuestID = availableQuests[i].questID
-              autoquestsRepeatable = availableQuests[i].repeatable
-              if nAvailable >= 2 and autoquestsRepeatable == true then
-                -- Nothing ... 
-              else
-                C_GossipInfo.SelectAvailableQuest(autoquestsQuestID)
-              end
-            end
-          end
-          if autoquestsRepeatable == true then
-            selfMessage(L.TITLE .. player .. L.REPEATABLE);
-          end
-        elseif nActive > 0 then
-          for i = 1, nActive do
-            if type(activeQuests) == "table" then
-              autoquestsIsComplete = activeQuests[i].isComplete
-              autoquestsQuestID = activeQuests[i].questID
-              if autoquestsIsComplete == true then
-                C_GossipInfo.SelectActiveQuest(autoquestsQuestID)
-              end
-            end
-          end
-        end
-      end
-    
-      if (event=="QUEST_GREETING") then
-        local npcAvailableQuestCount = GetNumAvailableQuests()
-        local npcActiveQuestCount = GetNumActiveQuests()
-    
-        if (npcAvailableQuestCount > 0) then
-          for i = 1, GetNumAvailableQuests() do
-            SelectAvailableQuest(i)
-          end
-        elseif (npcActiveQuestCount > 0) then
-          for i = 1, GetNumActiveQuests() do
-            SelectActiveQuest(i)
-          end
-        end
-      end
-    
-      if (event=="QUEST_DETAIL") then
-        AcceptQuest()
-      end
-    
-      if (event=="QUEST_PROGRESS") then
-        CompleteQuest()
-      end
-    
-      if (event=="QUEST_COMPLETE") then
-        local npcQuestRewardsCount = GetNumQuestChoices()
-        if (npcQuestRewardsCount > 1) then
-          selfMessage(L.TITLE .. player .. L.REWARD);
-          PlaySound(5274, "master")
-        else
-          GetQuestReward(default)
-        end
-      end
+local function AutoQuestsHandler(self, event, ...)
+    if not L.status or IsShiftKeyDown() or IsControlKeyDown() or IsAltKeyDown() then
+        return -- AutoQuests est désactivé ou une touche de modification est enfoncée
     end
-  end
 
-  local f = CreateFrame("Frame")
-    f:RegisterEvent ("MODIFIER_STATE_CHANGED")
-    f:RegisterEvent ("GOSSIP_SHOW")
-    f:RegisterEvent ("QUEST_PROGRESS")
-    f:RegisterEvent ("QUEST_DETAIL")
-    f:RegisterEvent ("QUEST_COMPLETE")
-    f:RegisterEvent ("QUEST_GREETING")
-    f:SetScript("OnEvent", Autoquests)
+    if event == "GOSSIP_SHOW" then
+        HandleGossipShow()
+    elseif event == "QUEST_GREETING" then
+        HandleQuestGreeting()
+    elseif event == "QUEST_DETAIL" then
+        AcceptQuest()
+    elseif event == "QUEST_PROGRESS" then
+        CompleteQuest()
+    elseif event == "QUEST_COMPLETE" then
+        HandleQuestCompletion()
+    end
 end
 
---[[
-    Load 
+--[[ 
+    Fonction pour gérer l'événement GOSSIP_SHOW
 ]]
+local function HandleGossipShow()
+    local nActive = C_GossipInfo.GetNumActiveQuests()
+    local activeQuests = C_GossipInfo.GetActiveQuests()
+    local nAvailable = C_GossipInfo.GetNumAvailableQuests()
+    local availableQuests = C_GossipInfo.GetAvailableQuests()
+    local gossipOptions = C_GossipInfo.GetOptions()
 
-SLASH_AUTOQUEST1 = "/autoquest";
-SLASH_AUTOQUEST2 = "/aq";
+    if #gossipOptions == 1 and gossipOptions[1].flags == 1 then
+        C_GossipInfo.SelectOption(gossipOptions[1].gossipOptionID)
+    else
+        local questOptionsCount = 0
+        for _, option in ipairs(gossipOptions) do
+            if option.flags == 1 then
+                questOptionsCount = questOptionsCount + 1
+            end
+        end
+        if questOptionsCount > 1 then
+            printMessage(L.TITLE .. playerName .. L.GOSSIP)
+            PlaySound(5274, "master")
+        elseif questOptionsCount == 1 then
+            C_GossipInfo.SelectOption(gossipOptions[1].gossipOptionID)
+        end
+    end
+
+    if nAvailable > 0 then
+        for i, quest in ipairs(availableQuests) do
+            if not quest.repeatable or nAvailable < 2 then
+                C_GossipInfo.SelectAvailableQuest(quest.questID)
+            end
+        end
+        if availableQuests[1].repeatable then
+            printMessage(L.TITLE .. playerName .. L.REPEATABLE)
+        end
+    elseif nActive > 0 then
+        for i, quest in ipairs(activeQuests) do
+            if quest.isComplete then
+                C_GossipInfo.SelectActiveQuest(quest.questID)
+            end
+        end
+    end
+end
+
+--[[ 
+    Fonction pour gérer l'événement QUEST_GREETING 
+]]
+local function HandleQuestGreeting()
+    local availableQuestsCount = GetNumAvailableQuests()
+    local activeQuestsCount = GetNumActiveQuests()
+
+    if availableQuestsCount > 0 then
+        for i = 1, availableQuestsCount do
+            SelectAvailableQuest(i)
+        end
+    elseif activeQuestsCount > 0 then
+        for i = 1, activeQuestsCount do
+            SelectActiveQuest(i)
+        end
+    end
+end
+
+--[[ 
+    Fonction pour gérer l'événement QUEST_COMPLETE 
+]]
+local function HandleQuestCompletion()
+    local rewardCount = GetNumQuestChoices()
+    if rewardCount > 1 then
+        printMessage(L.TITLE .. playerName .. L.REWARD)
+        PlaySound(5274, "master")
+    else
+        GetQuestReward(defaultRewardIndex)
+    end
+end
+
+--[[ 
+    Enregistrement des événements de quêtes 
+]]
+local function RegisterAutoQuestsEvents()
+    L.status = true
+    printMessage(L.TITLE .. L.ENABLE)
+
+    local frame = CreateFrame("Frame")
+    frame:RegisterEvent("GOSSIP_SHOW")
+    frame:RegisterEvent("QUEST_PROGRESS")
+    frame:RegisterEvent("QUEST_DETAIL")
+    frame:RegisterEvent("QUEST_COMPLETE")
+    frame:RegisterEvent("QUEST_GREETING")
+    frame:SetScript("OnEvent", AutoQuestsHandler)
+end
+
+--[[ 
+    Commandes slash pour activer/désactiver l'addon 
+]]
+SLASH_AUTOQUEST1 = "/autoquest"
+SLASH_AUTOQUEST2 = "/aq"
 
 SlashCmdList["AUTOQUEST"] = function(msg)
-  msg = string.lower(msg)
-  --	selfMessage(L.TITLE .. msg)
-	msg = { string.split(" ", msg) }
-	local exec
-	if #msg >= 1 then
-	  exec = table.remove(msg, 1)
-	end
-	if exec == "on" then
-		L.status = true
-		selfMessage(L.TITLE .. L.ENABLE)
-	elseif exec == "off" then
-		L.status = false
-		selfMessage(L.TITLE .. L.DISABLE)
-	else
-    if L.status == true then
-      L.statusReturn = L.ENABLE
+    msg = string.lower(msg)
+    local exec = msg:match("^(%S*)") -- Extraire la première partie de la commande
+
+    if exec == "on" then
+        L.status = true
+        printMessage(L.TITLE .. L.ENABLE)
+    elseif exec == "off" then
+        L.status = false
+        printMessage(L.TITLE .. L.DISABLE)
     else
-      L.statusReturn = L.DISABLE
+        local statusMessage = L.status and L.ENABLE or L.DISABLE
+        printMessage(L.TITLE .. L.STATE .. statusMessage)
     end
-		selfMessage(L.TITLE .. L.STATE .. L.statusReturn)
-	end
 end
 
-local f = CreateFrame("Frame")
-f:RegisterEvent("PLAYER_LOGIN")
-f:SetScript("OnEvent", function(self, event)
+--[[ 
+    Gestionnaire de l'événement de connexion du joueur 
+]]
+local loginFrame = CreateFrame("Frame")
+loginFrame:RegisterEvent("PLAYER_LOGIN")
+loginFrame:SetScript("OnEvent", function(self, event)
     if event == "PLAYER_LOGIN" then
-      RegisterAutoQuestsEvents()
+        RegisterAutoQuestsEvents()
     end
 end)
